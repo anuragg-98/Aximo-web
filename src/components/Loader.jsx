cd0b199fd4ce767879a432040f5c132f643a9e26
anuragg-98 <anuragsb22@gmail.com>
2025-07-21 16:30:49 +0530

    Create Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+const Loader = () => (
+  <div className="h-screen flex items-center justify-center">
+    <span className="animate-ping w-8 h-8 bg-white rounded-full"></span>
+  </div>
+);
+
+export default Loader;
