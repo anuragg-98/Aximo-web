241463ac52d04a292fea2990f7d9a7666aa134bd
anuragg-98 <anuragsb22@gmail.com>
2025-07-21 16:34:25 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Loader = () => (
   <div className="h-screen flex items-center justify-center">
     <span className="animate-ping w-8 h-8 bg-white rounded-full"></span>
